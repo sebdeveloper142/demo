3b49af0aa14427851c74c5ea1617823d938f32fa
sebdeveloper142 <seb_developer@hotmail.com>
2022-03-05 10:19:29 +0100

    Hellow Azure

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace demo_web_app
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
